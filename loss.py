c6f984cd1a668c460908b4e2098530411b86777b
QasimKhan5x <straightouttadetroit.qk@gmail.com>
2023-01-27 11:56:09 +0000

    loss function

--- loss.py
+++ loss.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch import linalg as LA
@@ -9,35 +10,59 @@ class DisruptionLoss(nn.Module):
     def __init__(self, config):
         super().__init__()
         
-        self.weights = torch.tensor([config.c1, config.c2, config.c3], dtype=torch.float32)
-        self.weights = nn.Parameter(self.weights)
         self.eps = config.eps
         self.order = config.order
         self.alpha = config.alpha
+        self.weights = None
         
-    def forward(self, attacked_x, gen_x, perturbed_x, 
-                    x_fake_perturbed, x_real_perturbed):
+    def set_weights(self, weights):
+        self.weights = weights
+    
+    def forward(self, x_perturbed_fake, x_fake, perturbation,
+                x_perturbed_fake_detected, x_perturbed_detected):
+        '''
+        x_perturbed_fake = G(x + P(x))
+        x_fake = G(x)
+        perturbation = P(x)
+        x_perturbed_fake_detected = D(G(x + P(x)))
+        x_perturbed_detected = D(x + P(x))
+        '''
+    
+        l1 = distortion_loss(x_perturbed_fake, x_fake, order=self.order)
+        l2 = perturbation_loss(perturbation, eps=self.eps)
+        l3 = detection_loss_fake(x_perturbed_fake_detected)
+        l4 = detection_loss_real(x_perturbed_detected)
         
-        l1 = distortion_loss(attacked_x, gen_x, order=self.order)
-        l2 = perturbation_loss(perturbed_x, eps=self.eps)
-        l3 = detection_loss_fake(x_fake_perturbed)
-        l4 = detection_loss_real(x_real_perturbed)
+        # print("l1=",l1)
+        # print("l2=",l2)
+        # print("l3=",l3)
+        # print("l4=",l4)
+        # print("weights=", self.weights)
         
         task_loss = torch.stack([l2, l3, l4])
         objective_loss =  -l1 + self.weights @ task_loss
         return objective_loss, task_loss
         
-    def gradNorm(self, shared_layer, task_loss, initial_task_loss):
+    def zero_grad(self):
+        self.weights.grad = self.weights.grad * 0.0
+    
+    def renormalize(self):
+        n_tasks = self.weights.size(0)
+        normalize_coeff = n_tasks / torch.sum(self.weights, dim=0)
+        self.weights.grad.data = self.weights.grad.data * normalize_coeff
+    
+    def gradNorm(self, model, task_loss, initial_task_loss):
         '''
         https://github.com/brianlan/pytorch-grad-norm/blob/master/train.py#96
         
-        shared_layer: last layer of Pertubation Generator
+        model: perturbation model
         task_loss: unweighted loss for perturbation, 
                    detection_loss_fake, detection_loss_real
         initial_task_loss: task_loss at epoch 0 (should be numpy array)
         '''
         if not isinstance(initial_task_loss, np.ndarray):
-            initial_task_loss = initial_task_loss.numpy()
+            initial_task_loss = initial_task_loss.detach().cpu().numpy()
+        shared_layer = list(model.children())[-1]
         # get the gradient norms for each of the tasks
         # G^{(i)}_w(t)
         norms = []
@@ -54,18 +79,18 @@ class DisruptionLoss(nn.Module):
         # compute the inverse training rate r_i(t)
         # \curl{L}_i
         if torch.cuda.is_available():
-            loss_ratio = task_loss.data.cpu().numpy() / initial_task_loss
+            loss_ratio = task_loss.detach().cpu().numpy() / initial_task_loss
         else:
-            loss_ratio = task_loss.data.numpy() / initial_task_loss
+            loss_ratio = task_loss.detach().numpy() / initial_task_loss
         # r_i(t)
         inverse_train_rate = loss_ratio / np.mean(loss_ratio)
         # print('r_i(t): {}'.format(inverse_train_rate))
 
         # compute the mean norm \tilde{G}_w(t)
         if torch.cuda.is_available():
-            mean_norm = np.mean(norms.data.cpu().numpy())
+            mean_norm = np.mean(norms.detach().cpu().numpy())
         else:
-            mean_norm = np.mean(norms.data.numpy())
+            mean_norm = np.mean(norms.detach().numpy())
         # print('tilde G_w(t): {}'.format(mean_norm))
 
         # compute the GradNorm loss
@@ -86,27 +111,32 @@ class DisruptionLoss(nn.Module):
         return grad_norm_loss
 
     
-def distortion_loss(attacked_x, gen_x, order):
+def distortion_loss(x_perturbed_fake, x_fake, order):
     '''
-    attacked_x = G(x + P(x))
-    gen_x = G(x)
+    x_perturbed_fake = G(x + P(x))
+    x_fake = G(x)
     '''
-    attacked_x = attacked_x.view(-1)
-    gen_x = gen_x.view(-1)
-    return LA.norm(attacked_x - gen_x, ord=order).mean()
+    x_perturbed_fake = x_perturbed_fake.view(-1)
+    # print(x_fake.shape)
+    x_fake = x_fake.view(-1)
+    difference = x_perturbed_fake - x_fake
+    # print(x_fake.shape)
+    # print("difference:", difference.mean())
+    norm = LA.norm(difference, ord=order)
+    return norm.mean()
 
 
-def perturbation_loss(perturbed_x, eps):
+def perturbation_loss(p_x, eps):
     '''
-    perturbed_x = P(x)
+    p_x = P(x)
     '''
-    perturbed_x = perturbed_x.view(-1)
-    return F.relu(LA.norm(perturbed_x) - eps)
+    p_x = p_x.view(-1)
+    return F.relu(LA.norm(p_x) - eps)
 
 
 def detection_loss_fake(z):
     '''
-    x_fake = G(x + P(x))
+    x_perturbed_fake = G(x + P(x))
     z = D(x_fake)
     ^ minimize predicted logits
     '''
@@ -115,7 +145,7 @@ def detection_loss_fake(z):
 
 def detection_loss_real(z):
     '''
-    z = 1 - D(x + P(x))
+    z = D(x + P(x))
     '''
     return (1 - z.view(-1)).mean()
 
@@ -162,6 +192,7 @@ if __name__ == "__main__":
     criterion = DisruptionLoss(config)
     loss = criterion(attacked_x, gen_x, perturbed_x, x_fake_perturbed, x_real_perturbed)
     print(loss.item())
+    print(criterion.we)
     
     # loss = disruption_loss(attacked_x, gen_x, perturbed_x, 
     #                        x_fake_perturbed, x_real_perturbed,
