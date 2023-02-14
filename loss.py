2365b51392d592f119f0ca2d07f91f83db4e46fd
QasimKhan5x <straightouttadetroit.qk@gmail.com>
2023-02-14 15:17:03 +0000

    fix loss

--- loss.py
+++ loss.py
@@ -12,14 +12,11 @@ class DisruptionLoss(nn.Module):
         
         self.eps = config.eps
         self.order = config.order
-        self.alpha = config.alpha
-        self.weights = None
         
-    def set_weights(self, weights):
-        self.weights = weights
     
     def forward(self, x_perturbed_fake, x_fake, perturbation,
-                x_perturbed_fake_detected, x_perturbed_detected):
+                x_perturbed_fake_detected, x_perturbed_detected,
+                weights):
         '''
         x_perturbed_fake = G(x + P(x))
         x_fake = G(x)
@@ -34,7 +31,7 @@ class DisruptionLoss(nn.Module):
         l4 = detection_loss_real(x_perturbed_detected)
         
         task_loss = torch.stack([l2, l3, l4])
-        objective_loss =  -l1 + self.weights @ task_loss
+        objective_loss = l1 + weights @ task_loss
         
         return {
             'objective_loss': objective_loss,
@@ -44,77 +41,8 @@ class DisruptionLoss(nn.Module):
             'detection_loss_fake': l3,
             'detection_loss_real': l4
         }
-        
-        
-    def zero_grad(self):
-        self.weights.grad = self.weights.grad * 0.0
-    
-    def renormalize(self):
-        n_tasks = self.weights.size(0)
-        normalize_coeff = n_tasks / torch.sum(self.weights, dim=0)
-        self.weights.grad.data = self.weights.grad.data * normalize_coeff
-    
-    def gradNorm(self, model, task_loss, initial_task_loss):
-        '''
-        https://github.com/brianlan/pytorch-grad-norm/blob/master/train.py#96
-        
-        model: perturbation model
-        task_loss: unweighted loss for perturbation, 
-                   detection_loss_fake, detection_loss_real
-        initial_task_loss: task_loss at epoch 0 (should be numpy array)
-        '''
-        if not isinstance(initial_task_loss, np.ndarray):
-            initial_task_loss = initial_task_loss.detach().cpu().numpy()
-        shared_layer = list(model.children())[-1]
-        # get the gradient norms for each of the tasks
-        # G^{(i)}_w(t)
-        norms = []
-        for i in range(len(task_loss)):
-            # get the gradient of this task loss with respect to the shared parameters
-            gygw = torch.autograd.grad(
-                task_loss[i], shared_layer.parameters(), retain_graph=True)
-            # compute the norm
-            norms.append(torch.norm(
-                torch.mul(self.weights[i], gygw[0])))
-        norms = torch.stack(norms)
-        # print('G_w(t): {}'.format(norms))
-
-        # compute the inverse training rate r_i(t)
-        # \curl{L}_i
-        initial_task_loss[initial_task_loss == 0] = 1e-5
-        if torch.cuda.is_available():
-            loss_ratio = task_loss.detach().cpu().numpy() / initial_task_loss
-        else:
-            loss_ratio = task_loss.detach().numpy() / initial_task_loss
-        # r_i(t)
-        inverse_train_rate = loss_ratio / np.mean(loss_ratio)
-        # print('r_i(t): {}'.format(inverse_train_rate))
-
-        # compute the mean norm \tilde{G}_w(t)
-        if torch.cuda.is_available():
-            mean_norm = np.mean(norms.detach().cpu().numpy())
-        else:
-            mean_norm = np.mean(norms.detach().numpy())
-        # print('tilde G_w(t): {}'.format(mean_norm))
-
-        # compute the GradNorm loss
-        # this term has to remain constant
-        constant_term = torch.tensor(
-            mean_norm * (inverse_train_rate ** self.alpha), requires_grad=False)
-        if torch.cuda.is_available():
-            constant_term = constant_term.cuda()
-        # print('Constant term: {}'.format(constant_term))
-        # this is the GradNorm loss itself
-        grad_norm_loss = torch.sum(torch.abs(norms - constant_term))
-        # print('GradNorm loss {}'.format(grad_norm_loss))
-
-        # compute the gradient for the weights
-        self.weights.grad = torch.autograd.grad(
-            grad_norm_loss, self.weights)[0]
-        
-        return grad_norm_loss
-
-    
+              
+     
 def distortion_loss(x_perturbed_fake, x_fake, order):
     '''
     x_perturbed_fake = G(x + P(x))
@@ -134,7 +62,8 @@ def perturbation_loss(p_x, eps):
     p_x = p_x.view(p_x.size(0), -1)
     norm = LA.norm(p_x, axis=1)
     diff = norm - eps
-    return diff.mean()
+    hinge = F.relu(diff)
+    return hinge.mean()
     
 
 def detection_loss_fake(z):
@@ -195,7 +124,7 @@ if __name__ == "__main__":
     criterion = DisruptionLoss(config)
     loss = criterion(attacked_x, gen_x, perturbed_x, x_fake_perturbed, x_real_perturbed)
     print(loss.item())
-    print(criterion.we)
+    # print(criterion.we)
     
     # loss = disruption_loss(attacked_x, gen_x, perturbed_x, 
     #                        x_fake_perturbed, x_real_perturbed,
